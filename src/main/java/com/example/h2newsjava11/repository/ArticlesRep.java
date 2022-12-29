151d2e587425e9ba4fee8f7690abf54c54396e82
Murzik2020ai <61930598+Murzik2020ai@users.noreply.github.com>
2022-12-29 17:37:19 +0300

    Delete ArticlesRep.java

--- src/main/java/com/example/h2newsjava11/repository/ArticlesRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.h2newsjava11.repository;
-import com.example.h2newsjava11.model.Article;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticlesRep extends CrudRepository<Article, Long> {
-}
