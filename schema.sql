40b3c95318071ab2f13824a5e68376ef2355135d
Murzik2020ai <61930598+Murzik2020ai@users.noreply.github.com>
2022-12-29 17:33:38 +0300

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS ARTICLES;
-
-CREATE TABLE ARTICLES(
-                        id BIGINT AUTO-INCREMENT PRIMARY KEY,
-                        news_head VARCHAR (250) NOT NULL,
-			news_body VARCHAR (250) NOT NULL,
-);
-
-
